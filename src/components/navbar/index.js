15b694ece02f980f17ba3ff4af6400d7405c9a5e
Sachin Gandhi <sachingandhi@live.com>
2022-04-08 23:47:05 +0100

    created appearance of navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,21 +1,33 @@
 import React from 'react'
 import {FaBars} from 'react-icons/fa'
-import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks} from './NavBarElements'
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavBarElements'
 
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo to="/"> Sachin Gandhi </NavLogo>
+          <NavLogo to="/"> TranquilFolds </NavLogo>
           <MobileIcon>
             <FaBars />
           </MobileIcon>
           <NavMenu>
+          <NavItem>
+              <NavLinks to ="aboutme">Who I am</NavLinks>
+            </NavItem>
             <NavItem>
-              <NavLinks to ="about">About</NavLinks>
+              <NavLinks to ="service">What you need</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to ="about">Why?</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to ="signup">Sign Up</NavLinks>
             </NavItem>
           </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Members Area</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
