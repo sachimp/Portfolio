9869d81f62d24d871907b828c09ecdd23a2399a6
Sachin Gandhi <sachingandhi@live.com>
2022-04-18 02:35:40 +0100

    added two more home object sections

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -4,8 +4,42 @@ export const homeObjOne = {
   lightText: true,
   lightTextDesc: true,
   topLine: 'Become Stress-Free',
-  headline: 'Short, regular sessions that leave you less stressed and more Zen.',
-  description: 'In our, we will engage in two-way interactive teaching and learning. Some have called it mutual therapy. Others have called it guru-friendship. I call it Origami Lifecoaching. I have 25 years of experience, and in 25 sessions you will become 25x more likely to succeed in your goals.',
+  headline: 'Short sessions that leave you less stressed and more Zen.',
+  description: 'In our sessions, we will engage in two-way interactive teaching and learning. Some have called it mutual therapy. Others have called it guru-friendship. I call it Origami Lifecoaching. I have 25 years of experience, and in 25 sessions you will become 25x more likely to succeed in your goals (and learn how to fold some beautiful traditional Japanese art).',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/friendship.svg').default,
+  alt: 'Origami swan',
+  dark: true,
+  primary: true,
+  darkText: false
+};
+
+export const homeObjTwo = {
+  id: 'abooutme',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Become Stress-Free',
+  headline: 'Short sessions that leave you less stressed and more Zen.',
+  description: 'In our sessions, we will engage in two-way interactive teaching and learning. Some have called it mutual therapy. Others have called it guru-friendship. I call it Origami Lifecoaching. I have 25 years of experience, and in 25 sessions you will become 25x more likely to succeed in your goals (and learn how to fold some beautiful traditional Japanese art).',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/friendship.svg').default,
+  alt: 'Origami swan',
+  dark: true,
+  primary: true,
+  darkText: false
+};
+
+export const homeObjThree = {
+  id: 'abooutme',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Become Stress-Free',
+  headline: 'Short sessions that leave you less stressed and more Zen.',
+  description: 'In our sessions, we will engage in two-way interactive teaching and learning. Some have called it mutual therapy. Others have called it guru-friendship. I call it Origami Lifecoaching. I have 25 years of experience, and in 25 sessions you will become 25x more likely to succeed in your goals (and learn how to fold some beautiful traditional Japanese art).',
   buttonLabel: 'Get Started',
   imgStart: false,
   img: require('../../images/friendship.svg').default,
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Sidebar from '../components/Sidebar'
 import Navbar from '../components/navbar'
 import HeroSection from '../components/HeroSection'
 import InfoSection from '../components/InfoSection'
-import { homeObjOne } from '../components/InfoSection/Data'
+import { homeObjOne, homeObjThree, homeObjTwo } from '../components/InfoSection/Data'
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -19,7 +19,10 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
-      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjOne} /> 
+      <InfoSection {...homeObjTwo} /> 
+      <InfoSection {...homeObjThree} /> 
+
     </>
   )
 }
