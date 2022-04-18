54ec6cb2d9a877882e0d3d63bf6651426ffefb74
Sachin Gandhi <sachingandhi@live.com>
2022-04-18 02:44:01 +0100

    styled new sections

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,5 +1,5 @@
 export const homeObjOne = {
-  id: 'abooutme',
+  id: 'aboutme',
   lightBg: false,
   lightText: true,
   lightTextDesc: true,
@@ -16,24 +16,24 @@ export const homeObjOne = {
 };
 
 export const homeObjTwo = {
-  id: 'abooutme',
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
+  id: 'service',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
   topLine: 'Become Stress-Free',
   headline: 'Short sessions that leave you less stressed and more Zen.',
   description: 'In our sessions, we will engage in two-way interactive teaching and learning. Some have called it mutual therapy. Others have called it guru-friendship. I call it Origami Lifecoaching. I have 25 years of experience, and in 25 sessions you will become 25x more likely to succeed in your goals (and learn how to fold some beautiful traditional Japanese art).',
   buttonLabel: 'Get Started',
-  imgStart: false,
-  img: require('../../images/friendship.svg').default,
+  imgStart: true,
+  img: require('../../images/people.svg').default,
   alt: 'Origami swan',
-  dark: true,
+  dark: false,
   primary: true,
-  darkText: false
+  darkText: true
 };
 
 export const homeObjThree = {
-  id: 'abooutme',
+  id: 'why',
   lightBg: false,
   lightText: true,
   lightTextDesc: true,
@@ -42,7 +42,7 @@ export const homeObjThree = {
   description: 'In our sessions, we will engage in two-way interactive teaching and learning. Some have called it mutual therapy. Others have called it guru-friendship. I call it Origami Lifecoaching. I have 25 years of experience, and in 25 sessions you will become 25x more likely to succeed in your goals (and learn how to fold some beautiful traditional Japanese art).',
   buttonLabel: 'Get Started',
   imgStart: false,
-  img: require('../../images/friendship.svg').default,
+  img: require('../../images/remote.svg').default,
   alt: 'Origami swan',
   dark: true,
   primary: true,
